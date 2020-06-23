d4d98459906ed5b0dddc2f5753218952aae9dbf9
woclass <inkydragon@users.noreply.github.com>
2020-06-23 18:09:48 +0800

    [zh] LaTeX Online buttons

--- zh/includes/buttons.js
+++ zh/includes/buttons.js
@@ -1,7 +1,7 @@
 var buttons ={
-    "edit":             "edit",
-    "copy":             "copy",
-    "Open in Overleaf": "Open in Overleaf",
-    "LaTeX Online":     "LaTeX Online",
-    "Delete Output":    "Delete Output"
+    "edit":             "编辑",
+    "copy":             "复制",
+    "Open in Overleaf": "在 Overleaf 上打开",
+    "LaTeX Online":     "在线编译 LaTeX",
+    "Delete Output":    "清空输出"
 }
